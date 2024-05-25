4fdd4e60d58e6da1725e1d9bc59b28759ddda63f
dgz <dagouzhi@qq.com>
2024-05-25 12:16:53 +0800

    fix

--- mini-apps-template-expo/src/utils/index.ts
+++ mini-apps-template-expo/src/utils/index.ts
@@ -8,33 +8,37 @@ export const minisdkRef =  React.createRef<any>();
 let storage = asyncStorage;
 
 if (Platform.OS !== 'web') {
-  const MMKV_KEY = `app_${__APP_DEFINE_APPID__}_storage`;
-  const Mmkv = require('react-native-mmkv').MMKV;
-  const ReactNativeBlobUtil = require('react-native-blob-util').default
-  const mmkvStorage = new Mmkv({
-    id: MMKV_KEY,
-    path: `${ReactNativeBlobUtil.fs.dirs.DocumentDir}/${MMKV_KEY}`,
-    encryptionKey: `${MMKV_KEY}_encryptionKey`
-  }) as MMKV;
-  storage = {
-    async getItem(key: string) {
-      const res = mmkvStorage.getString(key)
-      return res;
-    },
+  try {
+    const MMKV_KEY = `app_${__APP_DEFINE_APPID__}_storage`;
+    const Mmkv = require('react-native-mmkv').MMKV;
+    const ReactNativeBlobUtil = require('react-native-blob-util').default
+    const mmkvStorage = new Mmkv({
+      id: MMKV_KEY,
+      path: `${ReactNativeBlobUtil.fs.dirs.DocumentDir}/${MMKV_KEY}`,
+      encryptionKey: `${MMKV_KEY}_encryptionKey`
+    }) as MMKV;
+    storage = {
+      async getItem(key: string) {
+        const res = mmkvStorage.getString(key)
+        return res;
+      },
 
-    async setItem(key: string, data: string) {
-      return mmkvStorage.set(key, data)
-    },
+      async setItem(key: string, data: string) {
+        return mmkvStorage.set(key, data)
+      },
 
-    async removeItem(key: string) {
-      return mmkvStorage.delete(key)
-    },
+      async removeItem(key: string) {
+        return mmkvStorage.delete(key)
+      },
 
-    async clear(callback) {
-      mmkvStorage.clearAll()
-      callback?.();
-    }
-  } as AsyncStorageStatic
+      async clear(callback) {
+        mmkvStorage.clearAll()
+        callback?.();
+      }
+    } as AsyncStorageStatic
+  } catch (error) {
+    console.error('localStorage mmkv error: ', error)
+  }
 }
 
 export const localStorage: AsyncStorageStatic = storage;
