eb3ae906c7c53ba1773d4b74faac940295217b74
dgz <dagouzhi@qq.com>
2024-09-29 07:55:52 +0800

    fix

--- mini-apps-template/index.js
+++ mini-apps-template/index.js
@@ -3,6 +3,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import App from './src'
 import { useEffect } from 'react';
 import {MiniAppsEnginesProvider} from '@htyf-mp/engines'
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 
 // keep the splash screen visible while complete fetching resources
@@ -15,6 +16,8 @@ export default function Root() {
 
     }
   }, [])
-  return <MiniAppsEnginesProvider><App /></MiniAppsEnginesProvider>;
+  return <GestureHandlerRootView>
+    <SafeAreaProvider><MiniAppsEnginesProvider><App /></MiniAppsEnginesProvider></SafeAreaProvider>
+  </GestureHandlerRootView>;
 }
 /** 禁止修改此块代码 */
