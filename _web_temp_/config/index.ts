7fc40908dee31095e855ea56959fcace147640b5
dgz <dagouzhi@qq.com>
2026-03-25 17:02:38 +0800

    fix

--- _web_temp_/config/index.ts
+++ _web_temp_/config/index.ts
@@ -54,7 +54,7 @@ export default defineConfig<'webpack5'>(async (merge, { command, mode }) => {
       }
     },
     h5: {
-      publicPath: '/',
+      publicPath: 'auto',
       staticDirectory: 'static',
       output: {
         filename: 'js/[name].[hash:8].js',
