0f774b08c62eca80ef929c675afd1766cf4da54b
dgz <dagouzhi@qq.com>
2026-04-17 10:59:54 +0800

    fix ts

--- _apps_temp_/babel.config.js
+++ _apps_temp_/babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'react-native-worklets/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+  ],
 };
